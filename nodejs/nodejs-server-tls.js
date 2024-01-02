1b06bf2f7215e98311f07641470f8173161c803e
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-01-02 17:02:28 +0800

    Create nodejs-server-tls.js

---
+++ nodejs/nodejs-server-tls.js
@@ -0,0 +1,12 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt')
+};
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('Hello, world! TLS');
+}).listen(443);
