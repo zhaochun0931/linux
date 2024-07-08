31ab615b1fbe48e5dc4484022930a4c77c51f19f
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-07-08 15:45:46 +0800

    Create bash_position_parameters.sh

---
+++ linux-shell/bash_position_parameters.sh
@@ -0,0 +1,16 @@
+echo
+
+echo the file name is $0
+echo the 1st parameter is $1
+echo the 2nd parameter is $2
+echo the 3rd parameter is $3
+echo the 4th parameter is $4
+echo the 5th parameter is $5
+echo the 6th parameter is $6
+echo the 7th parameter is $7
+echo the 8th parameter is $8
+echo the 9th parameter is $9
+echo the 10th parameter is ${10}
+echo the 11th parameter is ${11}
+
+echo
