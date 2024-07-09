baf96bd5d12530b6d95819d32fb9d02f83c1f548
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-07-09 11:44:41 +0800

    Update bash-if-else.sh

--- linux-shell/bash-if-else.sh
+++ linux-shell/bash-if-else.sh
@@ -19,3 +19,52 @@ then
 else
        echo "xx.txt is not here"
 fi
+
+
+
+
+
+
+
+
+
+
+for i in 1 2 3
+do
+
+echo $i
+
+if test $i -eq 1;
+then
+echo exiting
+exit
+
+fi
+
+
+done
+
+
+
+
+
+
+
+
+for i in 1 2 3
+do
+
+echo $i
+
+if [ $i -eq 2 ];
+then
+echo exiting
+exit
+
+fi
+
+
+done
+
+
+
