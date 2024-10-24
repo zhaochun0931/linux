5b24845a280546540051bc5744695984c7f4413b
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-10-24 12:44:08 +0800

    Update bash-loop-for.sh

--- linux-shell/bash-loop-for.sh
+++ linux-shell/bash-loop-for.sh
@@ -1,27 +1,6 @@
-bash --version
 
 
 
-
-
-
-
-
-
-
-
-
-# higher bash version
-
-for i in {1..10}
-do
-echo $i
-done
-
-for i in {1..10..3}
-do
-echo $i
-done
 
 1
 4
@@ -33,21 +12,4 @@ done
 
 
 
-# C loop style
 
-for ((i=1; i<10; i++))
-do
-echo $i
-done
-
-
-
-
-
-
-# old bash version, the seq was out of date
-
-for i in $(seq 10)
-do
-echo $i
-done
