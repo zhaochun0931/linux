ddd4be5e5e2d9426885af42d8bf91f2f83231001
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-05 23:05:51 +0800

    Update and rename self-signed-SAN.sh to tls-self-signed-SAN.sh

---
+++ tls/tls-self-signed-SAN.sh
@@ -0,0 +1,14 @@
+SAN (Subject Alternative Name) is an extension in an SSL/TLS certificate that allows multiple domain names, subdomains, or IP addresses to be covered under a single certificate.
+
+
+1. Multi-Domain Support, Allows a certificate to secure multiple domains/subdomains.
+2. Avoids Common Name (CN) Limitations, The CN field only allows one hostname, but SAN allows many.
+3. Essential for Modern Browsers, Some browsers ignore the CN field and only use SAN.
+4. Used in Wildcard and Multi-Domain Certificates.
+
+
+
+
+
+
+
