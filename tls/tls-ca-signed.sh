eca3cdcddafd8726164ca82c30e6cf8d2996551d
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-05 22:26:00 +0800

    Update tls-ca-signed.sh

--- tls/tls-ca-signed.sh
+++ tls/tls-ca-signed.sh
@@ -1,1 +1,40 @@
+openssl req -x509 -new -nodes -keyout ca.key -out ca.crt -subj "/C=US/ST=State/L=City/O=MyCA/OU=IT/CN=MyCA"
+
+
+
+├── ca.crt
+└── ca.key
+
+
+
+
+
+openssl req -new -nodes -keyout ldap.key -out ldap.csr \
+-subj "/C=US/ST=State/L=City/O=Organization/OU=IT/CN=ldap.example.com"
+
+├── ca.crt
+├── ca.key
+├── ldap.csr
+└── ldap.key
+
+
+
+
+
+openssl x509 -req -in ldap.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out ldap.crt -days 365
+
+
+├── ca.crt
+├── ca.key
+├── ca.srl
+├── ldap.crt
+├── ldap.csr
+└── ldap.key
+
+
+
+
+
+
+
 
