26f266f5c2ea71675a300c15ae03ba2c3c2458a6
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-09-09 21:31:02 +0800

    Update tls-ca-signed-SAN.sh

--- tls/tls-ca-signed-SAN.sh
+++ tls/tls-ca-signed-SAN.sh
@@ -1,11 +1,3 @@
-openssl genrsa -out ca.key
-
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -out ca.crt -subj "/C=CN/ST=Shanghai/O=ORG/OU=ORG_UNIT/CN=myca"
-
-
-
-
-
 
 openssl req -new -nodes -keyout ldap.key -out ldap.csr -config san.cnf
 openssl x509 -req -in ldap.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out ldap.crt -days 365 -extfile san.cnf -extensions req_ext
@@ -35,6 +27,10 @@ Now you have:
 ca.key (CA private key)
 
 ca.crt (CA cert, trusted root)
+
+
+
+
 
 2️⃣ Generate Server Key + CSR with SAN
 
