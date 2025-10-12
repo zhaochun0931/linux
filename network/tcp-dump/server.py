37d1124122b3a06a0e0fa110f7ce84e1444d65a4
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-10-12 17:42:00 +0800

    Create server.py

---
+++ network/tcp-dump/server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = '0.0.0.0'
+PORT = 12345
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print("Received:", data.decode())
+            conn.sendall(data)  # Echo back
