067414c75c593d4b5f95415855e028edabb0a8f2
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-10-15 11:51:22 +0800

    Create condition.sh

---
+++ bash/condition.sh
@@ -0,0 +1,5 @@
+In Linux shell scripting (Bash), conditions are used in if, while, and until statements to control program flow.
+
+They typically appear inside [ ... ] or [[ ... ]], and evaluate to true or false depending on what you’re testing.
+
+
