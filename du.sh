062c387f40093c6d6098d4838887edcb9ba9af6b
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-11-03 15:04:01 +0800

    Update du.sh

--- du.sh
+++ du.sh
@@ -1,3 +1,19 @@
+The du command allows you to displays the amount of space that is being used by files in a directory.
+
+
+
+
+
+By default, the du command displays the disk usage in kilobytes.
+
+
+
+
+
+
+At the end of the list, the du command always shows the grand total for the current directory. To display only this information, supply the -s command line option:
+
+
 du -sh *
 
 du -sh * | sort -h
