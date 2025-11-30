818be77fd79f796ba101eff3787909fc4dff8f14
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-11-30 09:07:13 +0800

    Update app.js

--- nodejs/build-push-nodejs/app.js
+++ nodejs/build-push-nodejs/app.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const os = require('os');
 
+console.log("http server starting and listening on 8080...");
 
-
-console.log("hello server starting...");
 var handler = function(request, response) {
-console.log("Received request from " + request.connection.remoteAddress);
-response.writeHead(200);
-response.end("You've hit " + os.hostname() + "\n");
+  console.log("Received request from " + request.connection.remoteAddress);
+  response.writeHead(200);
+  response.end("You've hit " + os.hostname() + "\n");
 };
+
 var www = http.createServer(handler);
 www.listen(8080);
