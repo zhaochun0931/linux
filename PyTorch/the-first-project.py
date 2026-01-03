8fb47c77d9cd5e571024cd01f4af294851997d04
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2026-01-03 10:48:56 +0800

    Update the-first-project.py

--- PyTorch/the-first-project.py
+++ PyTorch/the-first-project.py
@@ -1,7 +1,7 @@
 import torch
 
 
-x = torch.arange(10)
+x = torch.arange(12)
 
 
 print(x)
@@ -9,5 +9,32 @@ print(x)
 
 print(x.shape)
 
-
 print(x.numel())
+
+
+
+
+
+
+X = x.reshape(3,4)
+
+print(X)
+
+
+
+
+
+
+y = torch.zeros(10)
+
+print(y)
+
+
+
+
+
+z = torch.ones(10)
+
+print(z)
+
+
